3def86eb6c93709f72bfeb84b32026601b7d26c9
Eirik Johansen <Eirik Johansen@5a9d8569-953f-0410-94e2-8fb92c326b53>
2010-01-07 13:55:08 +0000

    git-svn-id: https://netmaking.svn.beanstalkapp.com/apps/ezupgrade/trunk@586 5a9d8569-953f-0410-94e2-8fb92c326b53

--- classes/ezupgrade.php
+++ classes/ezupgrade.php
@@ -682,7 +682,7 @@ class eZUpgrade extends eZCopy
 		// they package their distros, but we use this for now
 		// Previously, we tried fetching the last created folder, but since the unpacked
 		// distro uses the date it was packed, this does not work
-		$this->data['new_distro_folder_name'] = $this->upgradeData['upgrade_base_path'] . $newDistroFolderName;
+		$this->data['new_distro_folder_name'] = $this->upgradeData['upgrade_base_path'] . $newDistroFolderName . '/';
 		
 		// $last_line = exec("cd " . $this->upgradeData['upgrade_base_path'] . ";ls -lrt | grep ^d");
 		// $this->data['new_distro_folder_name'] = rtrim(array_pop(preg_split("/[\s]+/", $last_line,-1,PREG_SPLIT_NO_EMPTY)), "\n");
