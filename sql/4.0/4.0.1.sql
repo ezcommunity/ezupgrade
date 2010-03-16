4f0b451b26d45533502a4f7bf47d5c231ef8bb01
Eirik Johansen <Eirik Johansen@5a9d8569-953f-0410-94e2-8fb92c326b53>
2010-03-16 13:44:23 +0000

    Removed indexes already applied as a part of the 3.10.1 upgrade.
    git-svn-id: https://netmaking.svn.beanstalkapp.com/apps/ezupgrade/trunk@858 5a9d8569-953f-0410-94e2-8fb92c326b53

--- sql/4.0/4.0.1.sql
+++ sql/4.0/4.0.1.sql
@@ -1,9 +1,6 @@
 SET storage_engine=INNODB;
 
-UPDATE ezsite_data SET value='4.0.0' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='4.0.1' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='5' WHERE name='ezpublish-release';
 DELETE FROM ezuser_setting where user_id not in (SELECT contentobject_id FROM ezuser);
 DELETE FROM ezcontentclass_classgroup WHERE NOT EXISTS (SELECT * FROM ezcontentclass c WHERE c.id=contentclass_id AND c.version=contentclass_version);
-ALTER TABLE ezcontent_language ADD INDEX ezcontent_language_name(name);
-ALTER TABLE ezcontentobject ADD INDEX ezcontentobject_owner(owner_id);
-ALTER TABLE ezcontentobject ADD UNIQUE INDEX ezcontentobject_remote_id(remote_id);
