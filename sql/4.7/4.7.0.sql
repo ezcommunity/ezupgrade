5bb41550e3ced756307604fcdeef40a76768cc85
Helge Silset <helge@netmaking.no>
2012-05-16 08:43:54 +0200

    Add final SQL for 4.7

--- sql/4.7/4.7.0.sql
+++ sql/4.7/4.7.0.sql
@@ -1,5 +1,5 @@
 SET storage_engine=InnoDB;
-UPDATE ezsite_data SET value='4.7.0alpha1' WHERE name='ezpublish-version';
+UPDATE ezsite_data SET value='4.7.0' WHERE name='ezpublish-version';
 UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
 
 
@@ -11,16 +11,11 @@ ALTER TABLE ezpending_actions ADD COLUMN id int(11) AUTO_INCREMENT PRIMARY KEY;
 -- the corresponding ezuser_accountkey record is not removed
 DELETE FROM ezuser_accountkey WHERE user_id IN ( SELECT user_id FROM ezuser_setting WHERE is_enabled = 1 );
 
-SET storage_engine=InnoDB;
-UPDATE ezsite_data SET value='4.7.0beta1' WHERE name='ezpublish-version';
-UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
-
 ALTER TABLE ezcontentobject_attribute MODIFY COLUMN data_float double default NULL;
 ALTER TABLE ezcontentclass_attribute MODIFY COLUMN data_float1 double default NULL;
 ALTER TABLE ezcontentclass_attribute MODIFY COLUMN data_float2 double default NULL;
 ALTER TABLE ezcontentclass_attribute MODIFY COLUMN data_float3 double default NULL;
 ALTER TABLE ezcontentclass_attribute MODIFY COLUMN data_float4 double default NULL;
 
-SET storage_engine=InnoDB;
-UPDATE ezsite_data SET value='4.7.0rc1' WHERE name='ezpublish-version';
-UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
+UPDATE eztrigger SET name = 'pre_updatemainassignment', function_name = 'updatemainassignment'
+  WHERE name = 'pre_UpdateMainAssignment' AND function_name = 'UpdateMainAssignment';
