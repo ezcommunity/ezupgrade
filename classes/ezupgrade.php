9c2fd48a89d2dd8fba10b81adfdc6b196682066d
Matti Mårds <matti@netmaking.no>
2012-11-27 10:54:09 +0100

    eZINI

--- classes/ezupgrade.php
+++ classes/ezupgrade.php
@@ -528,9 +528,8 @@ class eZUpgrade extends eZCopy
 			// get instance of current ini file
 			$ini = $this->iniInstance($iniFile, $basePath=$this->getNewDistroFolderName());
 
-
 			$oldDBName = false;
-			
+
 			// get current db name
 			if ( $ini->hasVariable( 'DatabaseSettings', 'Database' ) )
 			{
